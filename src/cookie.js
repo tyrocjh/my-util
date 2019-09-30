753bbc11c527517982f8bf38ce99432ad908cfc0
ECNetBIOS\yulu <danielwithss@gmail.com>
2019-09-30 17:16:10 +0800

    init prj

---
+++ src/cookie.js
@@ -0,0 +1,42 @@
+/**
+ * @desc 根据name读取cookie
+ * @param  {String} name
+ * @return {String}
+ */
+function getCookie(name) {
+    var arr = document.cookie.replace(/\s/g, "").split(';');
+    for (var i = 0; i < arr.length; i++) {
+        var tempArr = arr[i].split('=');
+        if (tempArr[0] == name) {
+            return decodeURIComponent(tempArr[1]);
+        }
+    }
+    return '';
+}
+
+/**
+ * @desc  设置Cookie
+ * @param {String} name
+ * @param {String} value
+ * @param {Number} days
+ */
+function setCookie(name, value, days) {
+    var date = new Date();
+    date.setDate(date.getDate() + days);
+    document.cookie = name + '=' + value + ';expires=' + date;
+}
+
+/**
+ * @desc 根据name删除cookie
+ * @param  {String} name
+ */
+function removeCookie(name) {
+    // 设置已过期，系统会立刻删除cookie
+    setCookie(name, '1', -1);
+}
+
+export default {
+    getCookie,
+    setCookie,
+    removeCookie
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import array from './array.js';
+import cookie from './cookie.js';
+
+export default {
+    array,
+    cookie
+}
