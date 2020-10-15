eaa5f363b0efda52ab0a90626e5016e0d8703c03
ECNetBIOS\yulu <danielwithss@gmail.com>
2020-10-15 12:23:08 +0800

    bug修复

--- src/device.js
+++ src/device.js
@@ -12,15 +12,15 @@ function getVersion() {
         webApp: !!ua.match(/Safari*/i), // 是否web应该程序，没有头部与底部
         weixin: !!ua.match(/MicroMessenger*/i), // 是否微信环境
         alipay: !!ua.match(/alipay*/i), // 是否支付宝环境
-        dd: !!ua.indexOf('dingtalk') > -1, // 是否钉钉浏览器
+        dd: !!(ua.indexOf('dingtalk') > -1), // 是否钉钉浏览器
         uc: !!ua.match(/UCBrowser*/i), // 是否uc浏览器
         qq: !!ua.match(/MQQBrowser*/i), // 是否uc浏览器
-        iPad: !!ua.indexOf('iPad') > -1, // 是否为iPad
+        iPad: !!(ua.indexOf('iPad') > -1), // 是否为iPad
         appVersion: app,
-        trident: !!ua.indexOf('Trident') > -1, // IE内核
-        presto: !!ua.indexOf('Presto') > -1, // opera内核
-        webKit: !!ua.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-        gecko: !!ua.indexOf('Gecko') > -1 && ua.indexOf('KHTML') == -1, // 火狐内核
+        trident: !!(ua.indexOf('Trident') > -1), // IE内核
+        presto: !!(ua.indexOf('Presto') > -1), // opera内核
+        webKit: !!(ua.indexOf('AppleWebKit') > -1), //苹果、谷歌内核
+        gecko: !!(ua.indexOf('Gecko') > -1) && ua.indexOf('KHTML') == -1, // 火狐内核
     };
 }
 
